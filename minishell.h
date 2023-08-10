b06a9e5f3be289b7b3c08edf9d32bd3309c665d1
Kekuhne <kekuhne@student.42Wolfsburg.de>
2023-08-10 17:15:11 +0200

    funtion for filling list is done. Lexer only handles | as token for now.

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: Kekuhne <kekuehne@student.42wolfsburg.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 12:20:00 by Kekuhne           #+#    #+#             */
-/*   Updated: 2023/08/03 15:59:59 by Kekuhne          ###   ########.fr       */
+/*   Updated: 2023/08/10 14:04:04 by Kekuhne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,9 @@
 typedef struct	s_lexer
 {
 	char	*raw_input;
-	char	*cmd;
-	char	*args;
+	char	**cmd;
 	char	*token;
-	struct	s_lexer *next;
-	struct	s_lexer *prev;
+	struct s_lexer	*next;
 }				t_lexer;
 
 typedef struct	s_envp
--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: Kekuhne <kekuehne@student.42wolfsburg.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 15:57:14 by Kekuhne           #+#    #+#             */
-/*   Updated: 2023/08/04 13:46:29 by Kekuhne          ###   ########.fr       */
+/*   Updated: 2023/08/10 17:12:50 by Kekuhne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	count_quotes(char *str, char qoutes)
 
 	i = 0;
 	found_c = 0;
-	while (str[i] && str[i] != '|')
+	while (str[i])
 	{
 		if (str[i] == qoutes)
 			found_c++;
@@ -37,7 +37,7 @@ int skip_quotes(char *str)
 	c = str[0];
 	while(str[i])
 	{
-		if (str[i] == c && !count_quotes(str + i + 1, c))
+		if (str[i] == c)
 			return (i);
 		else
 			i++;
@@ -46,22 +46,36 @@ int skip_quotes(char *str)
 	return (0);
 }
 
+int	check_quotes(char *str)
+{
+	int	double_quotes;
+	int	single_quotes;
+
+	double_quotes = count_quotes(str, '"');
+	single_quotes = count_quotes(str, '\'');
+	if (double_quotes == 0 && single_quotes == 0)
+		return (1);
+	if ((ft_strchr(str, '"') && double_quotes % 2 == 0)
+		|| (ft_strchr(str, '\'') && single_quotes % 2 == 0))
+		return(1);
+	else
+		return(0);
+}
+
 char *replace_whitespaces(char *str)
 {
 	int		i;
 
 	i = 0;
-	if ((ft_strchr(str, '"') && (count_quotes(str, '"') % 2 == 0))
-		|| (ft_strchr(str, '\'') && (count_quotes(str, '"') % 2 == 0)))
+	if (check_quotes(str))
 	{
 		while(str[i])
 		{
 			if(str[i] == '"' || str[i] == '\'')
 			{
 				i += skip_quotes(str + i);
-					i++;
 			}
-			if (str[i] == ' ')
+			if (str[i] == ' ' || str[i] == '\t')
 				str[i] = 26;
 			i++;
 		}
@@ -71,14 +85,138 @@ char *replace_whitespaces(char *str)
 	return (str);
 }
 
+int	char_count(char *str, char c)
+{
+	int	i;
+	int	found;
+	i = 0;
+	found = 0;
+	while (str[i])
+	{
+		if (str[i] == '"' || str[i] == '\'')
+			i += skip_quotes(str + i);
+		if (str[i] == c)
+			found++;
+		i++;
+	}
+	return(found);
+}
+
+void	add_new_node(t_lexer **root, t_lexer *new)
+{
+	t_lexer	*current;
+
+	current = *root;
+	while(current->next)
+		current = current->next;
+	current->next = new;
+}
+
+int	count_cmd(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i][0] != '|')
+	{
+		printf("str in count cmd is %s\n", str[i]);
+		i++;
+	}
+	printf("here2\n");
+	return (i);
+}
+
+t_lexer	*new_node(char **str)
+{
+	int	i;
+	int cmd_count; 
+	t_lexer	*node;
+
+	i = -1;
+	cmd_count = count_cmd(str);
+	printf("cmd count =  %d\n", cmd_count);
+	node = malloc(sizeof(t_lexer));
+	node->cmd = malloc(sizeof(char *) * cmd_count + 1);
+	while(++i < cmd_count)
+	{
+		node->cmd[i] = ft_strdup(str[i]);
+		if (node->cmd[i] == NULL)
+			return (NULL);
+		printf("new_node->cmd[%d] %s\n", i, node->cmd[i]);
+	}
+	node->cmd[cmd_count] = NULL;
+	if (str[i] && str[i][0] == '|')
+	{
+		node->token = ft_strdup(str[i]);
+		printf("token added %s\n", node->token);
+		if (node->token == NULL)
+			return(NULL);
+	}
+	else
+	{
+		node->token = NULL;
+		printf("token added (null)\n");
+	}
+	node->next = NULL;
+	return (node);
+}
+
+t_lexer	*lexer_list_init(char **str)
+{
+	int	i;
+	t_lexer *root;
+	t_lexer *node;
+
+	i = 0;
+	printf("counting throught split_str[%d] element\n", i);
+	root = new_node(str + i);
+	printf("new node added\n");
+	while(str[i][0] != '|' && str[i])
+		i++;
+	while(str[i++])
+	{
+		printf("counting throught split_str[%d] element\n", i);
+		printf("starting here %s\n", str[i]);
+		node = new_node(str + i);
+		if (node == NULL)
+			return (NULL);
+		add_new_node(&root, node);
+		printf("new node added\n");
+		printf("string before count_cmd %s\n", str[i]);
+		i += count_cmd(str + i);
+		printf("string after count_cmd %s\n", str[i]);
+		
+	}
+	return (root);
+}
+
 void	lexer(t_lexer *input)
 {
 	int i;
+	int j;
 	char **split_str;
 	
 	i = 0;
+	j = 0;
 	input->raw_input = replace_whitespaces(input->raw_input);
+	printf("raw string is %s\n", input->raw_input);
 	split_str = ft_split(input->raw_input, 26);
 	while (split_str[i])
-		printf("%s\n", split_str[i++]);
+		printf("split_str = '%s'\n", split_str[i++]);
+	input = lexer_list_init(split_str);
+	j = 0;
+	printf("*printing created linked list now*\n");
+	while (input)
+	{
+		i = 0;
+		while (input->cmd[i])
+		{
+			printf("cmd[%d] of %d note is : %s\n", i, j, input->cmd[i]);
+			i++;
+		}
+		printf("token of %d note is : %s\n", j ,  input->token);
+		j++;
+		input = input->next;
+	}
+	
 }
