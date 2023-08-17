e37dbb3acacccc0ee12f77dce8da81632a3500c2
yiwong <yiwong@student.42wolfsburg.de>
2023-08-17 20:41:36 +0200

    updated functions to take input correctly and accomodate signals

--- src/main.c
+++ src/main.c
@@ -17,35 +17,28 @@ char	*read_user_input(void)
 	return (readline("minishell > "));
 }
 
-void	shell_loop(char **envp)
+void	await_input(char **envp)
 {
-	t_lexer input;
-	
+	t_lexer	input;
+
 	if (!envp)
 		return ;
-	check_ctrl_c();
-	//scan for input ctrl+c, ctrl +d, ctrl + "\", directions. Up down for history,left/right for cursor.
-	// readline from input
 	input.raw_input = read_user_input();
 	lexer(&input);
-/* 	parser(line); */	
-		//execute commands (be mindful of built-in functions)
-	//free readline
 	free(input.raw_input);
-	//free arguments
-	//grap exit status from exec
 }
 
-int	main(int argc, char **argv, char **envp)
+int	main(int argument_count, char **arguments, char **envp)
 {
-	if (argc > 1)
+	global_signal_number = 0;
+	if (argument_count > 1)
 	{
-		perror("This program does not support input");
+		perror("This program does accept arguments\n");
 		exit(1);
 	}
-	if (!argv)
-		return (1);
-	shell_loop(envp);
-	//cleanup after exit of shell_loop;
+	(void)arguments; //to ignore "unused variable" error
+	await_signals();
+	while (!global_signal_number)
+		await_input(envp);
 	return (0);
 }
