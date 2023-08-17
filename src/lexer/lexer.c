dcfba0f03f6966de8b0f3e4e031803ad478a2fff
yiwong <yiwong@student.42wolfsburg.de>
2023-08-17 21:32:37 +0200

    mini exit and norm tidy

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -12,17 +12,19 @@
 
 #include "../../minishell.h"
 
-char *replace_whitespaces(char *str)
+char	*replace_whitespaces(char *str)
 {
 	int		i;
 
 	i = 0;
-	while(str[i])
+	if (!str)
+		return (NULL);
+	while (str[i])
 	{
-		if(str[i] == '"' || str[i] == '\'')
+		if (str[i] == '"' || str[i] == '\'')
 		{
 			i++;
-			while(str[i] != '"'|| str[i] == '\'')
+			while (str[i] != '"' || str[i] == '\'')
 			{
 				if (!str[i])
 					return (NULL);
@@ -30,7 +32,7 @@ char *replace_whitespaces(char *str)
 			}
 		}
 		if (str[i] == ' ')
-				str[i] = 26;
+			str[i] = 26;
 		i++;
 	}
 	return (str);
@@ -38,11 +40,16 @@ char *replace_whitespaces(char *str)
 
 void	lexer(t_lexer *input)
 {
-	int i = 0;
-	char **split_str;
-	
+	int		i;
+	char	**split_str;
+
+	i = 0;
 	input->raw_input = replace_whitespaces(input->raw_input);
+	if (!input->raw_input)
+		return ;
 	split_str = ft_split(input->raw_input, 26);
+	if (!ft_strncmp(split_str[0], "exit", 4))
+		quit(0);
 	while (split_str[i])
-	printf("%s\n", split_str[i++]);
+		printf("%s\n", split_str[i++]);
 }
