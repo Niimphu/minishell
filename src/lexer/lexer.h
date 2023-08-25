af55c95505fd8a99e46dad0b7431a56bb022365c
yiwong <yiwong@student.42wolfsburg.de>
2023-08-25 19:44:49 +0200

    removed silly

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 18:33:09 by yiwong            #+#    #+#             */
-/*   Updated: 2023/08/24 18:33:09 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/08/24 19:58:52 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include "../../minishell.h"
 
 char	*replace_whitespaces(char *str);
-int		skip_quotes(char *str);
 
 int		count_quotes(char *str, char quotes);
 
