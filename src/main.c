5e3a424421dfced81c082cdc4956dd51f0c745bd
yiwong <yiwong@student.42wolfsburg.de>
2023-08-25 15:30:38 +0200

    renamed tools to god_struct

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 12:17:49 by Kekuhne           #+#    #+#             */
-/*   Updated: 2023/08/22 17:18:56 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/08/24 20:13:58 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int	g_signal_received;
 
 int	main(int argument_count, char **arguments, char **envp)
 {
-	t_envp	*tools;
+	t_envp	*god_struct;
 
 	g_signal_received = 0;
-	tools = create_god_struct(envp);
+	god_struct = create_god_struct(envp);
 	if (argument_count > 1)
 	{
 		perror("This program does accept arguments\n");
@@ -29,7 +29,7 @@ int	main(int argument_count, char **arguments, char **envp)
 	while (g_signal_received != SIGQUIT)
 	{
 		await_signals();
-		await_input(tools);
+		await_input(god_struct);
 	}
 	return (0);
 }
