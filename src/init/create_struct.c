349189a6eadadb7a71515e87dbf25bf28dfb2c8c
yiwong <yiwong@student.42wolfsburg.de>
2023-08-25 19:43:31 +0200

    safer memory manipulation

--- src/init/create_struct.c
+++ src/init/create_struct.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 16:54:39 by yiwong            #+#    #+#             */
-/*   Updated: 2023/08/22 17:18:41 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/08/24 20:23:14 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,17 @@ t_envp	*create_god_struct(char **envp)
 	t_envp	*god_struct;
 
 	i = 0;
-	god_struct = malloc(sizeof(t_envp));
+	god_struct = ft_calloc(1, sizeof(t_envp));
 	if (!god_struct)
 		return (NULL);
-	god_struct->env = malloc(sizeof(char *));
+	god_struct->env = ft_calloc(count_cmd(envp) + 1, sizeof(char *));
+	if (!god_struct->env)
+		return (NULL);
 	while (envp[i])
 	{
-		god_struct->env[i] = malloc(sizeof(char) * ft_strlen(envp[i]));
+		god_struct->env[i] = ft_strdup(envp[i]);
 		if (!god_struct->env[i])
 			perror("upsi no envp!");
-		god_struct->env[i] = envp[i];
 		i++;
 	}
 	god_struct->lexer_list = NULL;
