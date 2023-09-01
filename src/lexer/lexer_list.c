68a359eaee5f225ab556ac4575e65dc8668bddf5
yiwong <yiwong@student.42wolfsburg.de>
2023-09-01 19:29:59 +0200

    prototype done

--- src/lexer/lexer_list.c
+++ src/lexer/lexer_list.c
@@ -12,49 +12,58 @@
 
 #include "../../minishell.h"
 
+static t_lexer	*new_lexer_node(char **array);
+static t_lexer	*fill_node(t_lexer *node, char **array);
+
 t_lexer	*create_lexer_list(char **split_string)
 {
 	t_list	*lexer_list;
 	t_lexer	*lexer_node;
-	int		operator_i;
+	char	*array_start;
 
+	array_start = split_string;
 	lexer_list = ft_lstnew((void *)lexer_node);
 	if (!lexer_list)
 		return (NULL);
-	lexer_node = new_lexer_node(split_string);
-	if (!lexer_node)
-		return (free(lexer_list), NULL);
-	
-	split_string += count_cmd(split_string);
-	if (!*split_string)
-		return (lexer_list);
-	populate_node(split_string);
+	while (split_string)
+	{
+		ft_lstadd_back(&lexer_list, new_lexer_node(split_string));
+		split_string += count_cmd(split_string);
+	}
+	free(array_start);
+	return (lexer_list);
 }
 
-t_lexer	*new_lexer_node(char **array)
+static t_lexer	*new_lexer_node(char **array)
 {
 	t_lexer	*lexer_node;
 
 	lexer_node = ft_calloc(1, sizeof(t_lexer));
 	if (!lexer_node)
 		return (NULL);
-	lexer_node->operator = NULL;
-	if (!contains_operator())
-	lexer_node->cmd = NULL;
+	fill_node(array);
 	return (lexer_node);
 }
 
-t_lexer	*populate_node(t_lexer *node, char **array)
+static t_lexer	*fill_node(t_lexer *node, char **array)
 {
-	
+	int		i;
+	int		cmd_count;
+
+	i = 0;
+	node->cmd = ft_calloc(count_cmd(array) + 1, sizeof(char *));
+	if (!node->cmd)
+		return (NULL);
+	cmd_count = count_cmd(array);
+	while (i < cmd_count)
+		node->cmd[i] = ft_strdup(array[i++]);
+	node->cmd[i] = NULL;
+	if (array[i])
+		node->operator = ft_strdup(array[i]);
+	else
+		node->operator = NULL;
+	return (node);
 }
-
-char **get_cmd_array(char **cmd_array)
-{
-	
-}
-
-
 
 
 static t_lexer	*new_node(char **str);
