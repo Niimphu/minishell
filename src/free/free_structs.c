67870676a4655be96479847c47e2ad7a0cf03163
yiwong <yiwong@student.42wolfsburg.de>
2023-09-08 15:49:38 +0200

    ladjflak

--- src/free/free_structs.c
+++ src/free/free_structs.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 17:41:11 by Kekuhne           #+#    #+#             */
-/*   Updated: 2023/09/08 15:42:26 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/09/08 15:48:34 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ void	free_node(void *node)
 	
 	parser_node = (t_parser *)node;
 	free_string_array(&parser_node->cmd);
+	free(node);
+	node = NULL;
 }
 
 void	free_god_struct(t_god **god_struct)
