3eb825e9f8c8de099198809b481e6bce54280601
yiwong <yiwong@student.42wolfsburg.de>
2023-09-20 17:22:11 +0200

    added static, removed old functions

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -22,9 +22,7 @@ char		*invalid_syntax(t_list *lexer_list);
 
 t_list		*create_lexer_list(char **input_array);
 t_list		*convert_commands(t_list *parser_list);
-//t_file	*file_away(t_lexer *current, t_lexer *next);
 t_parser	*file_away(t_parser *parser_node, t_lexer *current, t_lexer *next);
-t_list		*create_file_list(t_list *parser_list, t_list *lexer_list);
 
 int			get_operator_id(char *operator_string);
 
