1ba83690b4f3677afa3674d5480ae8f47f0a7b52
Kekuhne <kekuhne@student.42Wolfsburg.de>
2023-10-15 21:13:07 +0200

    stuffs happening

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,19 +6,7 @@
 /*   By: Kekuhne <kekuehne@student.42wolfsburg.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 15:06:05 by yiwong            #+#    #+#             */
-/*   Updated: 2023/10/15 20:35:57 by Kekuhne          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   unset.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/30 16:35:44 by Kekuhne           #+#    #+#             */
-/*   Updated: 2023/09/03 16:23:18 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/10/15 21:11:14 by Kekuhne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,12 +24,13 @@ int	unset(char **cmd, t_god *god_struct)
 	while (god_struct->env[i] && ft_strncmp(cmd[1], god_struct->env[i]
 			, first_index_of(god_struct->env[i], '=')))
 		i++;
-	while (god_struct->env[i])
+	free_string(&god_struct->env[i]);
+	while (god_struct->env[i + 1])
 	{
 		if (god_struct->env[i + 1])
-			god_struct->env[i] = ft_strdup(god_struct->env[i + 1]);
+			god_struct->env[i] = god_struct->env[i + 1];
 		i++;
 	}
-	free_string(&god_struct->env[i - 1]);
+	god_struct->env[i] = NULL;
 	return (0);
 }
