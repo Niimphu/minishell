b804e7c56487b1119ae85e70ab2d866ae6d8521f
yiwong <yiwong@student.42wolfsburg.de>
2023-10-15 20:45:42 +0200

    updated exit number

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -90,7 +90,7 @@ int	export(char **cmd, t_god *god_struct)
 	if (!cmd[i])
 		return (sort_env(god_struct, new_split_size(god_struct->env), 0, 0));
 	if (!verify_identifier("export", cmd[i]))
-		return (1);
+		return (2);
 	if (ft_strchr(cmd[i], '='))
 	{
 		god_struct->env = new_env(god_struct,cmd[i]);
