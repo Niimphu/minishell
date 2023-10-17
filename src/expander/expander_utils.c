3f08ac16ca4f003907ab2bf5d045867d32fcdd1d
yiwong <yiwong@student.42wolfsburg.de>
2023-10-17 23:09:07 +0200

    nicer

--- src/expander/expander_utils.c
+++ src/expander/expander_utils.c
@@ -60,8 +60,7 @@ char	*insert_sub_varlen(char *var, int i)
 	{
 		if (var[i] == '\'' || var[i] == '"')
 			quote = get_quote(var, i, quote);
-		if (var[i] == '$' && (var[i + 1] == '_'
-				|| ft_isalpha(var[i + 1])
+		if (var[i] == '$' && (var[i + 1] == '_' || ft_isalpha(var[i + 1])
 				|| var[i + 1] == '?') && quote != '\'')
 		{
 			var = insert_sub2(var, i);
